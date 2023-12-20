5a03dea591337112ea2ac4cf091b4a8b266c8842
Jack Punter <punter.jack@yahoo.com>
2023-12-20 00:01:08 +0000

    More models

--- benchmark/benchmark.c
+++ benchmark/benchmark.c
@@ -15,7 +15,7 @@ int mesh_to_v4_arr(HMM_Vec4 **vec, fastObjMesh *mesh) {
     assert(*vec); 
     
     HMM_Vec4 *cursor = *vec;
-    for (int i = 0; i < mesh->position_count; i += 3) {
+    for (int i = 0; i < mesh->position_count - 2; i += 3) {
         *cursor++ = HMM_V4(mesh->positions[i], mesh->positions[i + 1],
                            mesh->positions[i + 2], 1.0f);
         ++count;
@@ -27,16 +27,16 @@ double calc_secs(struct timespec start, struct timespec end) {
     return (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
 }
 
-int main() {
+int run_transform_test(const char *obj_file) {
     // Load the mesh
-    fastObjMesh *mesh = fast_obj_read("assets/stanford-bunny.obj");
+    fastObjMesh *mesh = fast_obj_read(obj_file);
     // printf("Loaded mesh with:\n  %d faces\n", mesh->face_count);
     // printf("  %d verts\n", mesh->position_count);
     
     // convert the mesh to HMM_Vec4s
     HMM_Vec4 *positions;
     int pos_count = mesh_to_v4_arr(&positions, mesh);
-    assert(pos_count - 1 == mesh->position_count / 3);
+    // assert(pos_count - 1 == mesh->position_count / 3);
     fast_obj_destroy(mesh);
     
     HMM_Mat4 look_at = HMM_LookAt_LH(HMM_V3(-1.0f, -1.0f, 1.0f),
@@ -63,12 +63,26 @@ int main() {
         
         HMM_Vec4 *tmp = transformed;
         transformed = positions;
-        positions = transformed;
+        positions = tmp;
         
         clock_gettime(CLOCK_MONOTONIC, &end);
     } while (calc_secs(start, end)< 10.0);
-    printf("Transformed the model %lu times in 10 seconds\n", counter);
     
-    // prevent the compiler from optimizing the array
+    free(positions);
+    free(transformed);
+    
+    printf("Transformed %lu times in 10 seconds (%s)\n", counter, obj_file);
     return random_x_sum;
+}
+
+int main() {
+    int variable = 0;
+    printf("Running transform test:\n");
+    
+    variable += run_transform_test("assets/suzanne.obj"); // 500
+    variable += run_transform_test("assets/teapot.obj"); //6320
+    variable += run_transform_test("assets/stanford-bunny.obj"); // 69.4k
+    variable += run_transform_test("assets/xyzrgb_dragon.obj"); // 1.1Mil
+    
+    return variable;
 }
